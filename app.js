d41c0e3a97cb216f7cb990487efd10c78aa6f04e
itsAmbika <sambika1101@gmail.com>
2026-03-29 15:17:12 +0530

    feat: add review functionality including schema, form UI, and backend route for persisting reviews

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const ejsMate=require("ejs-mate");
 const wrapAsync=require("./utils/wrapAsync.js");
 const ExpressError=require("./utils/ExpressError.js");
 const methodOverride=require("method-override");
+const { listingSchema } = require("./schema.js");
+const Review=require("./models/review.js");
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "/public")));
 const mongo_url="mongodb://127.0.0.1:27017/StayHub";
@@ -25,6 +27,14 @@ app.engine('ejs', ejsMate);
 app.get("/",(req,res)=>{
    res.send("hii");
 });
+const validateListing=(req,res,next)=>{
+    let {error}=listingSchema.validate(req.body);
+    if(error){
+        let errmsg=error.details.map(el=>el.message).join(",");
+        throw new ExpressError(400,errmsg);
+    }
+    next();
+}
 // index route
 app.get("/listings", wrapAsync(async(req,res)=>{
     const allListings=await Listing.find({});
@@ -35,10 +45,7 @@ app.get("/listings/new",(req,res)=>{
     res.render("listings/new");
 });
 //get post request from form
-app.post("/listings",wrapAsync(async(req,res,next)=>{
-     if(!req.body.listing){
-        throw new ExpressError(400,"Invalid listing data");
-     }
+app.post("/listings",validateListing,wrapAsync(async(req,res,next)=>{
      const newlisting=new Listing(req.body.listing);
      await newlisting.save();
      res.redirect("/listings");
@@ -57,10 +64,7 @@ app.get("/listings/:id",wrapAsync(async (req,res)=>{
 
 }));
 //edit data
-app.put("/listings/:id" ,wrapAsync(async (req,res)=>{
-    if(!req.body.listing){
-        throw new ExpressError(400,"Invalid listing data");
-     }
+app.put("/listings/:id" , validateListing, wrapAsync(async (req,res)=>{
     let {id}=req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
     res.redirect(`/listings/${id}`);
@@ -71,13 +75,25 @@ app.delete("/listings/:id", wrapAsync(async(req,res)=>{
      await Listing.findByIdAndDelete(id);
      res.redirect("/listings");
 }));
+
+//review route
+app.post("/listings/:id/reviews", wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    const review = new Review(req.body.review);
+    listing.reviews.push(review);
+    await review.save();
+    await listing.save();
+    res.redirect(`/listings/${id}`);
+}));
+
 app.all(/.*/ , (req,res,next)=>{
     next(new ExpressError(404,"Page Not Found!"));
 });
 
 app.use((err, req, res, next) => {
     let {status=500,message="Something went wrong"}=err;
-    res.status(status).send(message);
+    res.status(status).render("error",{message});
 });
 app.listen(8080, ()=>{
    console.log("server is listening");
