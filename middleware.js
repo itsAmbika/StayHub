b53d1e33254c8066240c17b55bdf369b8b443f36
itsAmbika <sambika1101@gmail.com>
2026-04-11 16:27:42 +0530

    feat: implement redirect to intended page after login

--- middleware.js
+++ middleware.js
@@ -1,8 +1,16 @@
 module.exports.isLoggedIn=(req,res,next)=>{
+    console.log(req);
     if(!req.isAuthenticated()){
+        req.session.redirectUrl=req.originalUrl;
         req.flash("error","You must be signed in!");
         return res.redirect("/login");
     }
     next();
 }
+module.exports.saveRedirectUrl=(req,res,next)=>{
+    if(req.session.redirectUrl){
+        res.locals.redirectUrl=req.session.redirectUrl;
+    }
+    next();
+};
 
--- routes/user.js
+++ routes/user.js
@@ -4,30 +4,33 @@ const User=require("../models/user.js");
 const wrapAsync=require("../utils/wrapAsync.js");
 const ExpressError=require("../utils/ExpressError.js");
 const passport=require("passport");
+const {saveRedirectUrl}=require("../middleware.js");
 router.get("/signup",(req,res)=>{
     console.log("signup route");
     res.render("users/signup.ejs");
 })
-router.post("/signup",async (req,res)=>{
-    try{
+router.post("/signup",wrapAsync(async (req,res,next)=>{
     let {username,email,password}=req.body;
-    newUser=new User({username,email});
-    user=await User.register(newUser,password);
-    console.log(user);
-    req.flash("success","Welcome to StayHub!");
-    res.redirect("/listings");}
-    catch(e){
-        req.flash("error",e.message);
-        res.redirect("/signup");
-    }
-});
+    const newUser=new User({username,email});
+    const user=await User.register(newUser,password);
+    req.login(user,(err)=>{
+        if(err){
+            return next(err);
+        }
+        req.flash("success","Welcome to StayHub!");
+        res.redirect("/listings");
+    });
+}));
 router.get("/login",(req,res)=>{
     res.render("users/login.ejs");
 });
 
-router.post("/login",passport.authenticate("local",{failureRedirect: "/login",failureFlash: true}), (req,res)=>{
+router.post("/login",
+    saveRedirectUrl,
+    passport.authenticate("local",{failureRedirect: "/login",failureFlash: true}), (req,res)=>{
     req.flash("success","Welcome to StayHub!");
-    res.redirect("/listings");
+    const redirectUrl = res.locals.redirectUrl || "/listings";
+    res.redirect(redirectUrl);
 });
 router.get("/logout",(req,res,next)=>{
     req.logout((err)=>{
