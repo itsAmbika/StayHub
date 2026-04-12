ee7183ac875091a9bab15c894389b5b429a1e1d1
itsAmbika <sambika1101@gmail.com>
2026-04-12 18:31:16 +0530

    feat: replace rating slider with star-based rating UI in review section

--- routes/listing.js
+++ routes/listing.js
@@ -4,7 +4,8 @@ const wrapAsync=require("../utils/wrapAsync.js");
 const Listing= require("../models/listing.js");
 const { isLoggedIn,isOwner,validateListing} = require("../middleware.js");
 const listingController=require("../controllers/listings.js");
-
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 router.route("/")
     .get(wrapAsync(listingController.index))
